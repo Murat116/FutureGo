dcc9746387cb6eb39a731dd01317ccaac6ceed66
Murat <89196242960@mail.ru>
2021-11-27 10:05:40 +0300

    Initial Commit

---
+++ Shared/FutureGoApp.swift
@@ -0,0 +1,17 @@
+//
+//  FutureGoApp.swift
+//  Shared
+//
+//  Created by Мурат Камалов on 11/27/21.
+//
+
+import SwiftUI
+
+@main
+struct FutureGoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
