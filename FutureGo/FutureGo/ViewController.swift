7c5a8711b9b7f3599845154614e796710aa70147
Roman <roma.shurkin@mail.ru>
2021-11-27 10:18:27 +0300

    new

---
+++ FutureGo/FutureGo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FutureGo
+//
+//  Created by Roman Shurkin on 27.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
